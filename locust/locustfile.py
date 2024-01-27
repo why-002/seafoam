8602d7f07b92e3e6ac9b98268a6339bdfb4fa1e7
wyatt <wyattrasmussen002@gmail.com>
2024-01-26 20:56:49 -0600

    changed testing to use randomly generated keys

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,13 +1,21 @@
-import time
+import time, random, string
 from locust import HttpUser, task, between, constant_throughput, FastHttpUser
 
 class QuickstartUser(FastHttpUser):
     wait_time = constant_throughput(1000)
-
+    current_keys = list()
     @task(9)
     def get(self):
-        self.client.post("/get", json={"key": "foo", "msg_id" : 0})
+        if len(self.current_keys) == 0:
+            self.client.post("/get", json={"key": "foo", "msg_id" : 0})
+        else:
+            self.client.post("/get", json={"key": random.choice(self.current_keys), "msg_id" : 0})
 
     @task
     def set(self):
-        self.client.post("/set", json={"key": "foo", "value": "baz", "msg_id" : 0})
+        if random.randint(0,10) % 10 == 0 or len(self.current_keys) == 0:
+            new = random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase)
+            self.current_keys.append(new)
+        else:
+            key = random.choice(self.current_keys)
+            self.client.post("/set", json={"key": key, "value": "baz", "msg_id" : 0})
