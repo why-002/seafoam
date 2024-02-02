4cedc4cf61dc0f1417cd43dee49014c9456968b2
wyatt <wyattrasmussen002@gmail.com>
2024-02-01 22:50:14 -0600

    updated locust concurrency to a state that should work for all testing

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,27 +1,24 @@
 import time, random, string
-from locust import HttpUser, task, between, constant_throughput, FastHttpUser, gevent
+from locust import HttpUser, task, between, constant_throughput, FastHttpUser
 
 class QuickstartUser(FastHttpUser):
     wait_time = constant_throughput(1000)
     current_keys = list()
+    concurrency = 100
+
     @task(9)
     def get(self):
-        def multipleGetRequests():
-            if len(self.current_keys) == 0:
-                self.client.post("/get", json={"key": "foo", "msg_id" : 0})
-            else:
-                self.client.post("/get", json={"key": random.choice(self.current_keys), "msg_id" : 0})
-        pool = gevent.pool.Pool(20)
-        for i in range(0,20):
-            pool.spawn(multipleGetRequests)
-        pool.join()
-
+        if len(self.current_keys) == 0:
+            self.client.post("/get", json={"key": "foo", "msg_id" : 0})
+        else:
+            self.client.post("/get", json={"key": random.choice(self.current_keys), "msg_id" : 0})
 
     @task
     def set(self):
         if random.randint(0,10) % 10 == 0 or len(self.current_keys) == 0:
             new = random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase)
             self.current_keys.append(new)
+            self.client.post("/set", json={"key": new, "value": "baz", "msg_id" : 0})
         else:
             key = random.choice(self.current_keys)
             self.client.post("/set", json={"key": key, "value": "baz", "msg_id" : 0})
