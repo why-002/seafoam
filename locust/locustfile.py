a99a6dd196c58c0bbc4ce2bc46ea5a6fa550cd0c
wyatt <wyattrasmussen002@gmail.com>
2024-02-01 18:19:13 -0600

    trying new concurrency with greenlets

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,15 +1,21 @@
 import time, random, string
-from locust import HttpUser, task, between, constant_throughput, FastHttpUser
+from locust import HttpUser, task, between, constant_throughput, FastHttpUser, gevent
 
 class QuickstartUser(FastHttpUser):
     wait_time = constant_throughput(1000)
     current_keys = list()
     @task(9)
     def get(self):
-        if len(self.current_keys) == 0:
-            self.client.post("/get", json={"key": "foo", "msg_id" : 0})
-        else:
-            self.client.post("/get", json={"key": random.choice(self.current_keys), "msg_id" : 0})
+        def multipleGetRequests():
+            if len(self.current_keys) == 0:
+                self.client.post("/get", json={"key": "foo", "msg_id" : 0})
+            else:
+                self.client.post("/get", json={"key": random.choice(self.current_keys), "msg_id" : 0})
+        pool = gevent.pool.Pool(20)
+        for i in range(0,20):
+            pool.spawn(multipleGetRequests)
+        pool.join()
+
 
     @task
     def set(self):
