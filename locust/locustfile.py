77862c2b07bbc8fcf562629c7bd7573b75f6b3e7
Wyatt <wyattrasmussen002@gmail.com>
2024-02-20 18:30:46 -0600

    updated load testing to properly set up when starting new clients

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -6,19 +6,20 @@ class QuickstartUser(FastHttpUser):
     current_keys = list()
     concurrency = 100
 
+    def on_start(self):
+        self.client.post("/set", json={"key": "bar", "value": "baz", "msg_id" : 0})
+        self.current_keys.append("bar")
     @task(9)
     def get(self):
-        if len(self.current_keys) == 0:
-            self.client.post("/get", json={"key": "foo", "msg_id" : 0})
-        else:
+        if len(self.current_keys) != 0:
             self.client.post("/get", json={"key": random.choice(self.current_keys), "msg_id" : 0})
 
     @task
     def set(self):
         if random.randint(0,10) % 10 == 0 or len(self.current_keys) == 0:
             new = random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase)
-            self.current_keys.append(new)
             self.client.post("/set", json={"key": new, "value": "baz", "msg_id" : 0})
+            self.current_keys.append(new)
         else:
             key = random.choice(self.current_keys)
             self.client.post("/set", json={"key": key, "value": "baz", "msg_id" : 0})
